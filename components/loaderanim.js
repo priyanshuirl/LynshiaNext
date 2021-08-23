3a5ed4eb08a903c83409c7dff2ac1903c89315de
Priyanshu Mishra <priyanshu12651265@gmail.com>
2021-08-23 22:46:06 +0530

    1.Fixed Loader not showing on nested links error

--- components/loaderanim.js
+++ components/loaderanim.js
@@ -3,7 +3,7 @@ import styles from "../styles/LoaderAnim.module.css"
 export default function LoaderAnim() {
     return (
         <div className={styles.loaderwrap}>
-            <img src="loader.svg"></img>
+            <img src="/loader.svg"></img>
         </div>
     )
 }
