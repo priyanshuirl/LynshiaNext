8b22e368f55904353814950bec0b954072875404
Priyanshu Mishra <priyanshu12651265@gmail.com>
2021-08-23 18:11:41 +0530

    1.Added 404 page

---
+++ pages/404.js
@@ -0,0 +1,39 @@
+import styles from "../styles/FourOFour.module.css"
+import { useRouter } from "next/router"
+import { useEffect, useState } from "react"
+
+export default function FourOFour() {
+    const [timer, setTimer] = useState(10);
+    var i = 10;
+    const router = useRouter()
+    useEffect(() => {
+        setInterval(() => {
+            setTimer(i);
+            i--
+        }, 1000)
+        setTimeout(() => {
+            router.push("/")
+        }, 13500)
+    }, [])
+    if (timer > 0) {
+        return (
+            <div className={styles.errorwrapper}>
+                <h1>404 error</h1>
+                <h2>The page you're looking for does not seem to exist.<br />
+                    Please check if you clicked or entered a valid URL.</h2>
+                <h3><br />But now that you're here,<br />
+                    We want you to smile&nbsp;<i class="far fa-laugh"></i></h3>
+                <h4>in . . . {timer}</h4>
+            </div>
+        )
+    } else {
+        return (
+            <div className={styles.errorwrapper}>
+                <h1>404 error</h1>
+                <h2>The page you're looking for does not seem to exist.<br />
+                    Please check if you clicked or entered a valid URL.</h2>
+                <h3><br />Redirecting To Homepage</h3>
+            </div>
+        )
+    }
+}
